35285e9dd94b194fd6783a6be05c24b5cf06b4b0
n8-des <nathan.desjardins@baxter-academy.org>
2017-10-16 10:33:16 -0400

    Reworked code
    Changed the code to match what it was meant to do

--- studentInfo.py
+++ studentInfo.py
@@ -1,38 +1,33 @@
 import random
 def main():
   students = [
-    Student("Larsson", "Halsted", 0, 1, 1, "Wukong", "top"),
-    Student("Desjardins", "Nate", 1, 30, 2, "Anivia", "mid"),
-    Student("Desalle", "Samson", 14, 2, 5, "Jhin", "adc"),
-    Student("Kreps", "Alex", 30, 15, 3, "Akali", "jungle"),
-    Student("Bonney", "Kent", 10, 4, 20, "Bard", "supp"),
+    Champion()
   ]
 
   printHeader()
   selection = getUserSelection()
   if selection == 0:
-    printStudentsByAge(students)
-  elif selection == 1:
-    pass
-  elif selection == 2:
-    pass
+    RandomizedChampions()
   else:
     print ("SELECTION NOT RECOGNIZED")
 
 
-class Student:
-  names = ['John','Halsted','Nate','Samson','Alex','Meme-Man','Garbage','Bob','Aatrox','Aaron','Peter','George','Kent','Jack','Billy','Mortimer','Rick','Carl','Alfred','Steven','Jerome','Jerimiah']
-  lNames = ['Bonjovi','Larsson','Desjardins','Dankness','Benson','McDepressed','Salad','Trashson','Stevenson','Weeabo','Namek','Salty','MacDonald','Tenderoni','Bonney','Kreps']
-  lanes = ['mid','adc', 'support', 'top','jungle']
+class Champion:
+  lanes = ['mid','adc','support','top','jungle']
   mainChamps = ['Aatrox','Ahri','Akali','Alistar','Amumu','Anivia','Annie','Ashe','Aurelion Sol','Azir','Bard','Blitzcrank','Brand','Braum','Caitlyn','Camille','Cassiopeia','ChoGath','Corki','Darius','Diana','Dr Mundo','Draven','Ekko','Elise','Evelynn','Ezreal','Fiddlesticks','Fiora','Fizz','Galio','Gangplank','Garen','Gnar','Gragas','Graves','Hecarim','Heimerdinger','Illoai','Irelia','Ivern','Janna','Jarvan IV','Jax','Jayce','Jhin','Jinx','Kalista','Karma','Karthus','Kassadin','Katarina','Kayle','Kennen','KhaZix','Kindred','Kled','KogMaw','LeBlanc','Lee Sin','Leona','Lissandra','Lucian','Lulu','Lux','Malphite','Malzahar','Maokai','Master Yi','Miss Fortune','Mordekaiser','Morgana','Nami','Nasus','Nautilus','Nidalee','Nocturne','Nunu','Olaf','Orianna','Pantheon','Poppy','Quinn','Rammus','RekSai','Renekton','Rengar','Riven','Rumble','Ryze','Sejuani','Shaco','Shen','Shyvana','Singed','Sion','Sivir','Skarner','Sona','Soraka','Swain','Syndra','Tahm Kench','Taliyah','Talon','Taric','Teemo','Thresh','Thresh','Tristana','Trundle','Tryndamere','Twisted Fate','Twitch','Udyr','Urgot','Varus','Vayne','Veigar','VelKoz','Vi','Viktor','Vladimir','Volibear','Warwick','Wukong','Xerath','Xin Zhao','Yasuo','Yorick','Zac','Zed','Ziggs','Zilean','Zyra']
-  def __init__(self, lastName, firstName, K, D, A, mainChamp, lane):
+  fullItems = ['Abyssal Mask','Adaptive Helm','Archangels Staff','Ardent Censer','Unholy Grail','Banner of Command','Banshees Veil','Blade of the Ruined King','Dead Mans Plate','Deaths Dance','Duskblade of Drakathaar','Edge of Night','Essence Reaver','Eye of Equinox','Eye of the Oasis','Eye of the Watcher','Face of the Mountain','Frost Queens Claim','Frozen Heart','Frozen Mallet','Gargoyle Stoneplate','Guardian Angel','Guinsoos Rageblade','Hextech GLP-800','Hextech Gunblade','Hextech Protobelt-01','Iceborn Gauntlet','Infinity Edge','Knights Vow','Liandrys Torment','Lich Bane','Locket of the Iron Solari','Lord Dominiks Regards','Ludens Echo','Manamune','Maw of Malmortius','Mejais Soulstealer','Mercurial Scimitar','Mikaels Crucible','Morellonomicon','Mortal Reminder','Nashors Tooth','Ohmwrecker','Phantom Dancer','Rabadons Deathcap','Randuins Omen','Rapid Firecannon','Ravenous Hydra','Redemption','Righteous Glory','Rod Of Ages','Ruby Sightstone','Runaans Hurricane','Rylais Crystal Scepter','Spirit Visage','Statikk Shiv','Steraks Gage','Sunfire Cape','Talisman of Ascension','The Black Cleaver','The Bloodthirstier','Thornmail','Titanic Hydra','Trinity Force','Void Staff','Warmogs Armor','Wits End','Yomuus Ghostblade','Zekes Convergence','Zhonyas Hourglass','ZZ-Rot Portal','','','','','','','','']
+  def __init__(self):
     self.assignRandomK()
     self.assignRandomD()
     self.assignRandomA()
-    self.randomFirstName()
-    self.randomLastName()
     self.randomChampion()
     self.randomLane()
+    self.randomItem1()
+    self.randomItem2()
+    self.randomItem3()
+    self.randomItem4()
+    self.randomItem5()
+    self.randomItem6()
 
   def randomLane(self):
      self.lane = random.choice(self.lanes)
@@ -45,56 +40,33 @@ class Student:
   def assignRandomD(self):
      self.D = random.randint(0,30)
   def assignRandomA(self):
-     self.A = random.randint(0,30)   
-
-  def assignRandomWeight(self, isMetric):
-    pass
-
-  def assignRandomHeight(self, isMetric):
-    pass
-  
-  def randomFirstName(self): 
-     self.firstName = random.choice(self.names) 
-  
-  def randomLastName(self):
-     self.lastName = random.choice(self.lNames)
+     self.A = random.randint(0,30)  
+  def randomItem1(self):
+    self.item1 = random.choice(self.fullItems)
+  def randomItem2(self):
+    self.item2 = random.choice(self.fullItems)  
+  def randomItem3(self):
+    self.item3 = random.choice(self.fullItems)
+  def randomItem4(self):
+    self.item4 = random.choice(self.fullItems)
+  def randomItem5(self):
+    self.item5 = random.choice(self.fullItems)
+  def randomItem6(self):
+    self.item6 = random.choice(self.fullItems)
 
 inputQuestions = [ 
-  "For PLAYERS BY DEATHS, type 0",
-  "For STUDENTS BY LAST NAME, type 1",
-  "For STUDENTS BY FIRST NAME, type 3",
-  "For SUM of STUDENT AGES type 4",
-  "For AVERAGE of STUDENT AGES type 5",
+  "For CHAMPIONS, type 0",
 ]
 
 def getUserSelection():
   print (inputQuestions[0])
-  print (inputQuestions[1])
-  print (inputQuestions[2])
   return int(input("Type selection and press enter: "))
 
+def RandomizedChampions():
+  count = int( input("How Many Randomized Champions? "))
+  for i in range(0, count):
+    champ = Champion()
+    print (str(champ.mainChamp) + " " + str(champ.lane) + ", " + str(champ.item1) + ", " + str(champ.item2) + ", " + str(champ.item3) + ", " + str(champ.item4) +", " + str(champ.item5) + ", " + str(champ.item6))
 def printHeader():
-    print("HEADER TEXT HERE")
-
-def printStudentsByAge(students):
-  print ("----Students By Age-----")
-  sortStudents = sorted(students, key=lambda student: student.D)
-  for student in sortStudents:
-    print (student.lastName + ", " + student.firstName + ", " + str(student.K) + "/" + str(student.D) + "/" + str(student.A) + ", " + str(student.mainChamp) + ", " + str(student.lane))
-def printStudentsByLName(students):
-  print ("----Students By -----")
-
-def printStudentsByFName(students):
-  print ("----Students By -----")
-
-def printSumAge(students):
-  print ("Answer:")
-
-def printAvgAge(students):
-  print ("Answer:")
-
-def ageRange(studentA, studentB):
-  return math.abs(studentA.age - studentB.age)
-
-
+    print("League Champion Build Generator")
 main()
